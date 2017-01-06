65eae956db0e0f11c4d53c1ae98e8671d1032346
Jessel94 <jesselakerveld@hotmail.com>
2017-01-06 23:34:42 +0100

    Fix url error

--- learn_english/exercises/urls.py
+++ learn_english/exercises/urls.py
@@ -20,7 +20,7 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
-    url(r"^exercise/(?P<name>verbs|adverbs|imperatives)/(?P<level>A1|A2|B1|B2|C1|.*)", views.exercise, name='exercise'),
+    url(r"^exercise/(?P<name>verbs|adverbs|imperatives|quantifiers)/(?P<level>A1|A2|B1|B2|C1|.*)", views.exercise, name='exercise'),
     url(r"^adverbs/(?P<level>A1|A2|B1|B2|C1|.*)", views.adverbs, name='adverbs'),
     url(r"^verbs/(?P<level>A1|A2|B1|B2|C1|.*)", views.verbs, name='verbs'),
     url(r"^imperatives/(?P<level>A1|A2|B1|B2|C1|.*)", views.imperatives, name='imperatives'),
