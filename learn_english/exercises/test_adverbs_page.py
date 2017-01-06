06f89e4c56264835c45b557f637cba1270cbc48d
LambdaNL <rene.van.der.horst@protonmail.com>
2017-01-06 14:16:22 +0100

    A2: 0.2.1

--- learn_english/exercises/test_adverbs_page.py
+++ learn_english/exercises/test_adverbs_page.py
@@ -7,7 +7,11 @@ class AdverbsTestCase(TestCase):
     def setUp(self):
         self.client = Client()
         models.OpenQuestion.objects.create(category="A1", question="He ... reads a book (quick):", answer="quickly")
-        models.FilledOpenQuestion.objects.create(username="foo", question="He ... reads a book (quick):", answer="quickly")
+        models.OpenQuestion.objects.create(category="A2", question="We watched ... (attentive):", answer="attentively")
+        models.FilledOpenQuestion.objects.create(username="foo", question="He ... reads a book (quick):",
+                                                 answer="quickly")
+        models.FilledOpenQuestion.objects.create(username="foo", question="We watched ... (attentive):",
+                                                 answer="attentively")
 
     def test_store_questions(self):
         """Test relevant models"""
@@ -17,19 +21,27 @@ class AdverbsTestCase(TestCase):
         self.assertNotEqual(open_question.question, "bar")
         self.assertNotEqual(filled_open_question.username, "baz")
 
-    def test_render_questions(self):
+    def test_render_questions_A1(self):
         """Test question rendering"""
         response = self.client.get('/adverbs/A1')
         content = response.content
         self.assertTrue("He ... reads a book (quick):" in str(content))
 
-    def test_answer_submission(self):
-        """Test submission of answers"""
+    def test_render_questions_A2(self):
+        """Test question rendering"""
+        response = self.client.get('/adverbs/A2')
+        content = response.content
+        self.assertTrue("We watched ... (attentive):" in str(content))
+
+    def test_answer_submission_A1(self):
+        """Test submission of answers A1"""
         self.client.post('/adverbs/A1', {'username': 'rare', '1': 'foo', '2': 'bar'})
         query = models.FilledOpenQuestion.objects.filter(username='rare')
         self.assertNotEqual(0, query.count())
 
-
-
-
+    def test_answer_submission_A2(self):
+        """Test submission of answers A2"""
+        self.client.post('/adverbs/A2', {'username': 'rare', '1': 'foo', '2': 'bar'})
+        query = models.FilledOpenQuestion.objects.filter(username='rare')
+        self.assertNotEqual(0, query.count())
 
